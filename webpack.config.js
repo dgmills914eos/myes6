354806ecb739664fea7cc937d1533a206481def1
Dan Mills <dgmills914eos@gmail.com>
2020-06-03 16:05:40 -0500

    adapted config file to app structure

--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,43 @@
-/**
- * Created by 
- */
+const path = require('path');
 module.exports = {
-
-  entry: './assets/js/scripts.js',
-  output: {filename: './public/js/bundle.js'
-
-
+  mode: 'development',
+  entry: path.join(__dirname, 'app', 'index'),
+  watch: true,
+  output: {
+    path: path.join(__dirname, 'dist'),
+    publicPath: '/dist/',
+    filename: "bundle.js",
+    chunkFilename: '[name].js'
   },
-
-
-  module:{
-
-      loaders: [
-
-
-          {
-
-              test: /\.js$/,
-              exclude: /node_modules/,
-              loader: 'babel-loader',
-              query: {
-
-                  presets:['es2015']
-
-              }
-
-
-
-
-          }
-
-
-
-      ]
-
-
+  module: {
+    rules: [{
+      test: /.jsx?$/,
+      include: [
+        path.resolve(__dirname, 'app')
+      ],
+      exclude: [
+        path.resolve(__dirname, 'node_modules')
+      ],
+      loader: 'babel-loader',
+      query: {
+        presets: [
+          ["@babel/env", {
+            "targets": {
+              "browsers": "last 2 chrome versions"
+            }
+          }]
+        ]
+      }
+    }]
+  },
+  resolve: {
+    extensions: ['.json', '.js', '.jsx']
+  },
+  devtool: 'source-map',
+  devServer: {
+    contentBase: path.join(__dirname, '/dist/'),
+    inline: true,
+    host: 'localhost',
+    port: 8080,
   }
-
-
-
-
-
 };
