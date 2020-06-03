bcfd5026eb5d7a2793ea72567d813a4a53ec423f
Dan Mills <dgmills914eos@gmail.com>
2020-06-03 15:47:03 -0500

    tweaking

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,45 @@
+/**
+ * Created by 
+ */
 module.exports = {
-    entry: './assets/js/scripts.js',
-    output: {
-        filename: './public/js/bundle.js'
-    },
 
-    module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                exclude:/node_modules/,
-                loader: 'babel-loader',
-                query: {
-                    presets:['es2015']
-                }
-            }
-        ]
-    }
+  entry: './assets/js/scripts.js',
+  output: {filename: './public/js/bundle.js'
 
-   
+
+  },
+
+
+  module:{
+
+      loaders: [
+
+
+          {
+
+              test: /\.js$/,
+              exclude: /node_modules/,
+              loader: 'babel-loader',
+              query: {
+
+                  presets:['es2015']
+
+              }
+
+
+
+
+          }
+
+
+
+      ]
+
+
+  }
+
+
+
+
 
 };
